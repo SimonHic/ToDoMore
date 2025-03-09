f22ca8cc001ebd66f5c48eb3955f2d63257a6d58
SimonHic <simonhicdev@gmail.com>
2025-03-09 22:14:53 +0000

    Add About Screen content and remove redundant code in AppNavGraph.

--- app/src/main/java/ie/setu/todomore/ui/screens/about/AboutScreen.kt
+++ app/src/main/java/ie/setu/todomore/ui/screens/about/AboutScreen.kt
@@ -7,5 +7,5 @@ import androidx.compose.ui.Modifier
 // Screen to give a brief explanation of the app
 @Composable
 fun AboutScreen(modifier: Modifier = Modifier){
-    Text(text = "About Screen")
+    Text(text = "ToDo More is a task management app that allows users to add, edit, and organise tasks with priority levels. MORE COMING SOON!")
 }
